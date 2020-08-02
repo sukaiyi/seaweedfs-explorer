c96ac6a97db462828455895cfecf84cf244d6ce9
sukaiyi <1433855681@qq.com>
2020-08-02 13:27:55 +0800

    build: 优化打包过程，jar 包最小化

--- src/main/java/com/sukaiyi/seaweedfsexplorer/Application.java
+++ src/main/java/com/sukaiyi/seaweedfsexplorer/Application.java
@@ -2,11 +2,28 @@ package com.sukaiyi.seaweedfsexplorer;
 
 import com.sukaiyi.seaweedfsexplorer.nettyserver.ServerBootstrapConfig;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author sukaiyi
  * @date 2020/07/31
  */
 public class Application {
+
+    /**
+     * 该 static 块引用一些运行时需要的 Class， 以避免他们在打包时被 maven-shade-plugin 删除（配置了minimizeJar=true）
+     */
+    static {
+        List<Class<?>> classesShouldNotBeRemoved = Arrays.asList(
+                org.apache.log4j.ConsoleAppender.class,
+                org.apache.log4j.PatternLayout.class
+        );
+        System.out.println("Class that should not be remove by maven-shade-plugin:");
+        classesShouldNotBeRemoved.stream()
+                .map(Class::getName)
+                .forEach(System.out::println);
+    }
 
     public static void main(String[] args) {
 
